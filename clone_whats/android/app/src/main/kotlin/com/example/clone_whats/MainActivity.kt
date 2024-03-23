c175f1d8f798cc3738010bde455aecda8c2e8bd1
Anderson Dutra <andersonrdutra7@gmail.com>
2024-03-22 21:50:23 -0300

    Criado Projeto Clone Whats

---
+++ clone_whats/android/app/src/main/kotlin/com/example/clone_whats/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clone_whats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ clone_whats/lib/main.dart
@@ -0,0 +1,182 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WhatsApp Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xff075E54),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xff25D366)),
+      ),
+      home: const WhatsAppHome(),
+    );
+  }
+}
+
+class WhatsAppHome extends StatefulWidget {
+  const WhatsAppHome({super.key});
+
+  @override
+  _WhatsAppHomeState createState() => _WhatsAppHomeState();
+}
+
+class _WhatsAppHomeState extends State<WhatsAppHome> {
+  List<Map<String, dynamic>> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadUsers();
+  }
+
+  Future<void> loadUsers() async {
+    try {
+      String jsonString = await rootBundle.loadString('assets/users.json');
+      setState(() {
+        users = json.decode(jsonString)['users'];
+      });
+    } catch (e) {
+      print('Erro ao carregar os dados do usuário: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("WhatsApp", style: TextStyle(color: Colors.white)),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.green,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.camera_alt, color: Colors.white),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon:
+                      const Text("Chat", style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Text("Status",
+                      style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Text("Calls",
+                      style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                Widget trailing = const Text("10:00");
+                if (index < 2) {
+                  trailing = Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      const Text("10:00"),
+                      Positioned(
+                        bottom: -12,
+                        child: Container(
+                          width: 24,
+                          height: 24,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green.withOpacity(0.4),
+                                spreadRadius: 2,
+                                blurRadius: 3,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }
+
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.green,
+                    child: Text("${user['firstName'][0]}"),
+                  ),
+                  title: Text("${user['firstName']} ${user['lastName']}"),
+                  subtitle: const Text("Previa da mensagem"),
+                  trailing: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text("10:00"),
+                      if (index < 2)
+                        Container(
+                          margin: const EdgeInsets.only(top: 4),
+                          width: 16,
+                          height: 16,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.green.withOpacity(0.4),
+                                spreadRadius: 2,
+                                blurRadius: 3,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                  ),
+                  onTap: () {},
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green.shade700,
+        shape: const CircleBorder(),
+        child: const Icon(Icons.message),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
