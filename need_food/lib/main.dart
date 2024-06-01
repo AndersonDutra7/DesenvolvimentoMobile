d690e2cf2a7a54992aa7700c4d70b228436e4e9d
Anderson Dutra <andersonrdutra7@gmail.com>
2024-06-01 00:15:20 -0300

    Realizado configuração de conexão com Firebase

--- need_food/lib/main.dart
+++ need_food/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:need_food/views/home_page.dart';
 import 'package:need_food/views/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:need_food/services/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Correção aqui
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const LoginPage(),
     );
   }
